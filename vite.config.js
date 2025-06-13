951b77db563557196f11339847ef588130628557
muhammed <adhilnizar86@gmail.com>
2025-06-14 02:29:09 +0530

    fixing refreshing yaaaa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 👈 ensures paths are resolved correctly
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+});
